d4e147c47f9a5e329792d1ef3657cacaeb54bdc2
Mauro Scomparin <scompo@gmail.com>
2014-07-18 00:57:16 +0200

    Added log..
    Signed-off-by: Mauro Scomparin <scompo@gmail.com>

--- src/main/java/it/scompo/checktest/ObjectContainerImpl.java
+++ src/main/java/it/scompo/checktest/ObjectContainerImpl.java
@@ -4,23 +4,41 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 import java.util.Map.Entry;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import it.scompo.checktest.exceptions.AlreadyExistentObjectTest;
 import it.scompo.checktest.exceptions.MissingIdException;
 import it.scompo.checktest.exceptions.ObjectTestNotFoundException;
 
 public class ObjectContainerImpl implements ObjectContainer {
 
+	final static Logger LOGGER = LogManager
+			.getLogger(ObjectContainerImpl.class);
+
 	private Map<Long, ObjectTest> data = new LinkedHashMap<Long, ObjectTest>();
 
 	public ObjectTest getObjectById(Long id) throws ObjectTestNotFoundException {
+
+		LOGGER.info("Called getObjectById with id: {}", id);
+
+		ObjectTest res = null;
+
 		if (!data.containsKey(id)) {
 			throw new ObjectTestNotFoundException();
 		}
-		return data.get(id);
+
+		res = data.get(id);
+
+		LOGGER.debug("Returning: {}", res);
+		return res;
 	}
 
 	public ObjectTest getObjectByFields(String dato1, String dato2)
 			throws ObjectTestNotFoundException {
+
+		LOGGER.info("Called getObjectByFields with dato1: {}, dato2: {}",
+				dato1, dato2);
 
 		ObjectTest res = null;
 
@@ -38,11 +56,15 @@ public class ObjectContainerImpl implements ObjectContainer {
 		if (res == null) {
 			throw new ObjectTestNotFoundException();
 		}
+
+		LOGGER.debug("Returning: {}", res);
 		return res;
 	}
 
 	public void addObject(ObjectTest object) throws AlreadyExistentObjectTest,
 			MissingIdException {
+
+		LOGGER.info("Called addObject with addObject: {}", object);
 
 		if (object.getId() == null) {
 			throw new MissingIdException();
